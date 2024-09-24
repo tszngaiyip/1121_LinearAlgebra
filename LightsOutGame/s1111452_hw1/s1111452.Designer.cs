8061ff48daba88799b9e20b64f0980b9e1d531ac
tszngaiyip <ytn2004725@gmail.com>
2024-09-24 14:34:19 +0800

    Create LightsOutGame

---
+++ LightsOutGame/s1111452_hw1/s1111452.Designer.cs
@@ -0,0 +1,23 @@
+﻿namespace s1111452_hw1{
+    partial class s1111452{
+        private System.ComponentModel.IContainer components = null;
+        protected override void Dispose(bool disposing){
+            if (disposing && (components != null)) components.Dispose();
+            base.Dispose(disposing);
+        }
+        #region Windows Form 設計工具產生的程式碼
+        private void InitializeComponent(){
+            this.SuspendLayout();
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(800, 800);
+            this.ForeColor = System.Drawing.SystemColors.Control;
+            this.Name = "s1111452";
+            this.Text = "789Form1";
+            this.Load += new System.EventHandler(this.s1111452_Load);
+            this.ResumeLayout(false);
+        }
+        #endregion
+    }
+}
---
+++ LightsOutGame/s1111452_hw1/s1111452.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace s1111452_hw1{
+    public partial class s1111452 : Form{
+        private Button[] buttons;
+        public s1111452(){
+            this.InitializeComponent();
+        }
+        private void ToggleColor(Button button){
+            button.BackColor = (button.BackColor == Color.Red) ? Color.Blue : Color.Red;
+        }
+        private void Button_Click(object sender, EventArgs e){
+            int index = int.Parse(((Button)sender).Text);
+            ToggleColor(buttons[index]);
+            if (index % 5 != 0) ToggleColor(buttons[index - 1]);
+            if (index % 5 != 4) ToggleColor(buttons[index + 1]);
+            if (index > 4) ToggleColor(buttons[index - 5]);
+            if (index < 20) ToggleColor(buttons[index + 5]);
+        }
+        private void s1111452_Load(object sender, EventArgs e){
+            this.buttons = new Button[25];
+            for (int i = 0; i < 25; ++i){
+                this.buttons[i] = new Button();
+                this.buttons[i].BackColor = i == 12 ? Color.Blue : Color.Red;
+                this.buttons[i].Location = new Point(100 * (i % 5), 100 * (i / 5));
+                this.buttons[i].Size = new Size(100, 100);
+                this.buttons[i].Text = i.ToString();
+                this.buttons[i].Click += new EventHandler(this.Button_Click);
+                base.Controls.Add(this.buttons[i]);
+            }
+        }
+    }
+}
